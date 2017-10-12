70efb81908f9196bf926c066d3c1deaf67a4c4c7
unknown <anthony-hamlin@att.net>
2017-10-12 18:56:34 -0400

    first commit

---
+++ Print.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">    
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+var userInput = prompt:("Would you like to print your name" > 'yes');
+
+if{
+var answer = > 'yes'  output:("Hello. My name is Tony");
+console.log("Hello. My name is Tony");
+}
+while userInput = prompt:("Would you like to print this again?" > 'yes');
+var  answer = > 'yes' output:(Hello. My name is Tony!');
+console.log("Hello. My name is Tony!");
+var userInput = prompt:("Would you like to print this again?" > 'yes');
+
+if{
+var  answer = > 'yes' output:(Hello. My name is Tony!!');
+console.log("Hello. My name is Tony!!");
+
+}
+var userInput = prompt:("Would you like to print this again > 'no');
+
+<body>
+<script src="index.js"></script>                        
+    
+</body>
+</html>
---
+++ index.js
@@ -0,0 +1,6 @@
+var total = ("66");
+var number = prompt:('"enter a number!" > 5; Output: 66)
+for (var num=1; num<11; num++){
+console.log (num);
+total + = num change total = total + num
+}*
